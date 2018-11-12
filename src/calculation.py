5d6f51c322b7dcf2fedcb377299ea344ab32bbbd
kabhel <helene.kabbech@gmail.com>
2018-11-12 16:36:52 +0100

    Calculate distances and correlations matrices only once and store in csv files

--- src/calculation.py
+++ src/calculation.py
@@ -6,7 +6,28 @@
 # Third-party modules
 import numpy as np
 
-def calculate_transcription_map(coordinates, correlations, nb_genes, gene):
+def calculate_correlation_matrix(expressions, path):
+    correlations = expressions.transpose().corr(method="spearman")
+    correlations.to_csv(path)
+
+
+def calculate_distance_matrix(coordinates, path):
+    for gene in coordinates.index:
+        coordinates_copy = coordinates.copy()
+        coordinates_copy.loc[gene] = np.NaN
+        # Calculation of the distance of the current gene with all genes
+        new_dist = np.sqrt((coordinates_copy['X'] - coordinates['X'][gene])**2 +\
+                           (coordinates_copy['Y'] - coordinates['Y'][gene])**2 +\
+                           (coordinates_copy['Z'] - coordinates['Z'][gene])**2)
+        new_dist.name = gene
+        try:
+            dist =  pd.concat([dist, new_dist], axis=1, join_axes=[dist.index])
+        except:
+            dist = new_dist
+        correlations.to_csv(path)
+
+
+def calculate_transcription_map(distances, correlations, nb_genes, gene):
     """
     Finds the n closest genes of a current gene and calcules the transcription map corresponding.
 
@@ -19,18 +40,11 @@ def calculate_transcription_map(coordinates, correlations, nb_genes, gene):
     Returns:
         float: The transcription map of the studied gene
     """
-    # NaN values are set instead of the coordinate values of the current gene
-    # because the distance does not need to be calculated
-    coordinates_copy = coordinates.copy()
-    coordinates_copy.loc[gene] = np.NaN
-    # Calculation of the distance of the current gene with all genes
-    distances = np.sqrt((coordinates_copy['X'] - coordinates['X'][gene])**2 +\
-                        (coordinates_copy['Y'] - coordinates['Y'][gene])**2 +\
-                        (coordinates_copy['Z'] - coordinates['Z'][gene])**2)
+    distances_gene = distances[gene]
     # The distances are sorted and the n smallest are selected
     # The corresponding genes represent the closest genes of the current gene
-    distances = distances.sort_values(ascending=True, na_position='last')
-    closest_genes = distances[:nb_genes].index
+    distances_gene = distances_gene.sort_values(ascending=True, na_position='last')
+    closest_genes = distances_gene[:nb_genes].index
     # The transcription map is calculated for the closest genes
     transcription_map = sum(abs(correlations[gene][closest_genes]))
     return transcription_map
