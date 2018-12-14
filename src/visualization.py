8a36b6d943e13aa34a42ebd3c5324eaf5b3988c0
kabhel <helene.kabbech@gmail.com>
2018-12-14 15:55:46 +0100

    Interactive plot using plotly

--- src/visualization.py
+++ src/visualization.py
@@ -4,30 +4,45 @@
 """
 
 # Third-party modules
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.cm as cmx
-import matplotlib.pyplot as plt
-import matplotlib.colors as col
+# import plotly.plotly as py
+import os
+import plotly
+import plotly.graph_objs as go
 
-def visualize_4d_genome(transcription_map, coordinates, colors_map='YlOrRd'):
+def visualize_4d_genome(coordinates, transcription_map, colors_map='Reds'):
     """
     Plot a 3D visualization of the genes colored by their transcription map.
 
     Args:
+        coordinates (Pandas Dataframe): X, Y, Z Coordinates of the genes    
         transcription_map (list): Transcription map for the studied genes
-        coordinates (Pandas Dataframe): X, Y, Z Coordinates of the genes
-        colors_map (str): Chosen colors of the color bar. Default: YlOrRd (Yellow or Red)
+        colors_map (str): Chosen colors of the color bar. Default: Reds
     """
-    col_map = plt.get_cmap(colors_map)
-    col_norm = col.Normalize(vmin=min(transcription_map), vmax=max(transcription_map))
-    scalar_map = cmx.ScalarMappable(norm=col_norm, cmap=col_map)
-    fig = plt.figure()
-    axes = Axes3D(fig)
-    axes.scatter(coordinates['X'], coordinates['Y'], coordinates['Z'],\
-                 c=scalar_map.to_rgba(transcription_map), marker='o')
-    scalar_map.set_array(transcription_map)
-    fig.colorbar(scalar_map)
-    axes.set_xlabel('X Label')
-    axes.set_ylabel('Y Label')
-    axes.set_zlabel('Z Label')
-    plt.show()
+    trace = go.Scatter3d(
+        x = coordinates['X'],
+        y = coordinates['Y'],
+        z = coordinates['Z'],
+        mode = 'markers',
+        marker = dict(
+            size = 4,
+            color = transcription_map,
+            colorscale = colors_map,
+            opacity = 0.8,
+            showscale=True
+        ),
+        text = "gene: " + coordinates.index + "<br>chr: " + coordinates[' chr']
+    )
+
+    layout = go.Layout(
+        title = "<b>3d Transcription Map</b>",
+        margin = dict(
+            l = 65,
+            r = 50,
+            b = 65,
+            t = 90
+        )
+    )
+    fig = go.Figure(data=[trace], layout=layout)
+    # py.iplot(fig, filename='3d transcription map')
+    os.makedirs('result', exist_ok=True)
+    plotly.offline.plot(fig, filename='result/3d_transcription_map.html')
